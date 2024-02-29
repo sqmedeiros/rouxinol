f92f44465b80a8803ec0b80e3af43c472dbd47b7
sqmedeiros <sqmedeiros@gmail.com>
2024-02-29 10:05:10 -0300

    Renaming only files with prefix "entry_"

--- scripts/rename_files.py
+++ scripts/rename_files.py
@@ -9,7 +9,10 @@ files = os.listdir(path)
 for file in files:
   s = path + "/" + file
   entry_pos = s.find(text)
-  print(f"file = {s}")
   
-  os.rename(s, s[:entry_pos] + s[entry_pos + len(text):])
-
+  if not entry_pos == -1:
+    print(f"Renaming file: {s}")
+    os.rename(s, s[:entry_pos] + s[entry_pos + len(text):])
+  else:
+    print(f"Not renaming file: {s}")
+  
