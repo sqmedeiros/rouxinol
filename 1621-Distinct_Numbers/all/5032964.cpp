0ff360109ef9764a09f54c2266270e0bb3a269e8
sqmedeiros <sqmedeiros@gmail.com>
2024-03-01 09:58:04 -0300

    Adding 50 solutions to 1621-Distinct_Numbers

---
+++ 1621-Distinct_Numbers/all/5032964.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    set<int>nums;
+    for(int i=0; i<n; i++){
+        int x;
+        cin>>x;
+        nums.insert(x);
+    }
+    cout<<nums.size();
+}
