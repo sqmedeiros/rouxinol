c8fbc3a23983f84400bc01d68db232cb8cfe28db
Sergio Medeiros <sqmedeiros@gmail.com>
2024-03-05 08:39:19 -0300

    Sorting list of solutions to pick

--- scripts/pick_rand_solutions.py
+++ scripts/pick_rand_solutions.py
@@ -8,10 +8,11 @@ page_last_solution = int(input("Page of the last C++ solution: "))
 
 n_solutions = int(input("Number of solutions to gather: "))
 
+solutions = []
 for _ in range(n_solutions):
   page = random.randint(page_first_solution, page_last_solution)
-  
   solution = random.randint(1, solutions_per_page)
+  solutions.append([page, solution])
   
-  print(f"{page} {solution}")
-
+for sol in sorted(solutions):
+  print(f"{sol[0]} {sol[1]}")
