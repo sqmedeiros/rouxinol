6216c7c159141d4cf9a7f8ad8a6c7b50782cd1a0
Sergio Medeiros <sqmedeiros@gmail.com>
2024-03-21 10:20:45 -0300

    Replacing 3187447 in rand directory

--- 1621-Distinct_Numbers/rand/3187447.cpp
+++ 1621-Distinct_Numbers/rand/3187447.cpp
@@ -8,7 +8,7 @@ Fenwick tree mein testcase ke andar wali max value size ko add() function mein r
 #define int long long
 #define ll long long
 #pragma GCC optimize("O3") //how good is this? lol
-//#pragma GCC target("sse,sse2,sse3,sse4,popcnt,fma")
+#pragma GCC target("avx,avx2,sse,sse2,sse3,sse4,popcnt,fma")
 #pragma GCC optimize("unroll-loops")
 using namespace std;
 const int MAXN=2e3+5;
@@ -31,3 +31,50 @@ int powerm(int a,int b) { //m-> mod
     }
     return res;
 }
+ 
+class facm {
+public:
+    vector<int>fac,inv;
+    int n=0,mod;
+    facm(int x,int M) { //precomputes factorials!
+        n=x;
+        mod=M;
+        fac.resize(n+1),inv.resize(n+1); //till fac[n] and inv[n]
+        fac[0]=1;
+        for(int i=1;i<=n;i++) {
+            fac[i]=(fac[i-1]*i)%mod;
+        }
+    }
+    void preinv() { //precomputes inverse! Thanks Aman for that trick! :P
+        inv[n]=powerm(fac[n],mod-2);
+        for(int i=n-1;i>=0;i--) {
+            inv[i]=(inv[i+1]*(i+1))%mod;
+        }
+    }
+    int choose(int a,int b) { //get nCr
+        if(a<b || a<0 || b<0) {
+            return 0;
+        }
+        return ((fac[a]*inv[b])%mod*inv[a-b])%mod;
+    }
+};
+ 
+ 
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    //cout<<setprecision(9)<<fixed;
+    //freopen("closing.in","r",stdin);
+    //freopen("closing.out","w",stdout);
+    int n,num;
+    cin>>n;
+    set<int>s;
+    for(int i=0;i<n;i++) {
+        cin>>num;
+        s.insert(num);
+    }
+    cout<<s.size();
+    return 0;
+}
+ 
+ 
