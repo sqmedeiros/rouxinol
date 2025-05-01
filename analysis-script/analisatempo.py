92e968d76e69d71dd566c81518e100c6f3e96cd0
borgesnogueira <borgesnogueira@gmail.com>
2025-05-01 08:10:58 -0300

    grafico com problemas no eixo x e labels

--- analysis-script/analisatempo.py
+++ analysis-script/analisatempo.py
@@ -4,11 +4,7 @@ Created on Sat May 13 09:26:47 2023
 
 @author: Marcelo
 
-Lists all files in a directory and creat a dataset for weka with all the dataset
-The atributes of the dataset will be the given by the metrics in the first line of the csv of each file (they must be equal in every file)
-cpu-cycles,instructions,cache-references,cache-misses,branch-instructions,branch-misses,page-faults,branch-loads,branch-load-misses,L1-dcache-loads,L1-dcache-load-misses,L1-dcache-stores,L1-icache-load-misses,LLC-loads,LLC-load-misses,LLC-stores,LLC-store-misses,dTLB-loads,dTLB-load-misses,dTLB-stores,dTLB-store-misses,iTLB-loads,iTLB-load-misses,user_time,system_time
-The class will be the number of the problem
-1071, 1082,...
+cira boxplot com tempo de execucao de varias solucoes
 """
 
 from signal import signal
@@ -58,6 +54,7 @@ vd = np.zeros(len(lista))
 vmax = np.zeros(len(lista))
 vmin = np.zeros(len(lista))
 dataset = []
+labels = []
 for i in lista:
      vprob[cont]=cont
      df, ncolunas, nlinhas = carregacsv(i)
@@ -70,14 +67,20 @@ for i in lista:
      desvio = tempo.std()
      vd[cont]=desvio
      cont += 1
-     
+     labels.append(i[0:4])
      
 
 
 #plt.errorbar(vprob,vm, yerr=nsigma*vd,fmt ='.r')
 #plt.plot(vprob,vmax,'.b')
 #plt.plot(vprob,vmin,'.g')
-plt.boxplot(dataset, positions=vprob,showfliers=False)
+fig, ax = plt.subplots()
+ax.boxplot(dataset, positions=vprob,showfliers=False)
+plt.xlabel('Problem')
+ax.set_xticks(vprob, labels, rotation='vertical') 
+plt.title('Wall clock boxplot')
+
+plt.ylabel('Wall clock time (ms)')
 
 plt.show()
      
